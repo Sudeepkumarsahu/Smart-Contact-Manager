105a95d7030c4e08452c4c746e4444736c1798a0
sudeep <sahusudeepkumar3@gmail.com>
2025-10-09 17:00:08 +0530

    form validation

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.scm.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -70,12 +72,15 @@ public class PageController {
 
     // processing register form data
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegisterForm(@ModelAttribute UserForm userForm, HttpSession session) {
+    public String processRegisterForm(@Valid @ModelAttribute UserForm userForm,BindingResult rBindingResult, HttpSession session) {
         System.out.println("processing registration");
         // fetch form data
         // UserForm
         System.out.println(userForm);
         // validate form data
+        if(rBindingResult.hasErrors()){
+            return "register";
+        }
         // sava data to database
         // // userForm se user banaya
         // User user = User.builder()
@@ -87,23 +92,23 @@ public class PageController {
         // .profilePicture("https://plus.unsplash.com/premium_photo-1739786996022-5ed5b56834e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWF0aW9uJTIwYXZhdGFyfGVufDB8fDB8fHww")
         // .build();
 
-      User user =  new User();
-      user.setName(userForm.getName());
-      user.setAbout(userForm.getAbout());
-      user.setEmail(userForm.getEmail());
-      user.setPassword(userForm.getPassword());
-      user.setPhoneNumber(userForm.getPhoneNumber());
-      user.setProfilePicture("https://plus.unsplash.com/premium_photo-1739786996022-5ed5b56834e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWF0aW9uJTIwYXZhdGFyfGVufDB8fDB8fHww");
-       User savedUser = userService.saveUser(user);
-       System.out.println("user saved");
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setAbout(userForm.getAbout());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePicture(
+                "https://plus.unsplash.com/premium_photo-1739786996022-5ed5b56834e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWF0aW9uJTIwYXZhdGFyfGVufDB8fDB8fHww");
+        User savedUser = userService.saveUser(user);
+        System.out.println("user saved");
         // message to user about successful registration
-        //add the message 
-      Message message =  Message.builder().content("Registration Successful").type(MessageType.green).build();
+        // add the message
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
 
-session.setAttribute("message",message);
+        session.setAttribute("message", message);
         // redirect to login page
-        return "redirect:/register";// Redirect to registration page after successful registration 
+        return "redirect:/register";// Redirect to registration page after successful registration
 
-        
     }
 }
--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +17,17 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserForm {
+    @NotBlank(message = "Username is reqired")
+    @Size(min = 3, message="Min 3 Character is required")
     private String name;
+    @Email(message = "invalid Email Address")
+    @NotBlank(message = "Email is reqired")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Min 6 Character is required")
     private String password;
+    @NotBlank(message = "About is required")
     private String about;
+    @Size(min = 8,max = 12,message = "Invalid Phone Number")
     private String phoneNumber;
 }
